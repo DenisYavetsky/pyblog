a4551db6146781c75f357b4bd330b9dac0f1c648
Denis <47447276+DenisYavetsky@users.noreply.github.com>
2019-12-15 13:21:00 +0300

    images

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from blog import views
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf.urls.static import static
 
-
-urlpatterns = [
+urlpatterns\
+    = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', views.posts_list, name='posts_list_url'),
     path('faq/', views.faq, name='faq_url'),
@@ -10,6 +14,14 @@ urlpatterns = [
     path('post/<str:slug>/', views.post_detail, name='post_detail_url'),
     path('category/<str:slug>/', views.category_detail, name='category_detail_url'),
     path('tag/<str:slug>/', views.tag_detail, name='tag_detail_url'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT
+        }),
+    ]
 
