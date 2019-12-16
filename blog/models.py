b15f95c2333556e85ecebf0a249d30a61e2907da
Denis <47447276+DenisYavetsky@users.noreply.github.com>
2019-12-16 21:06:03 +0300

    req

--- blog/models.py
+++ blog/models.py
@@ -34,7 +34,7 @@ class Post(models.Model):
     date_pub = models.DateField(auto_now_add=True)
     categories = models.ManyToManyField('Category', blank=True, related_name='posts')
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
-    img = models.ImageField(upload_to='', blank=True)
+    img = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
         return '{}'.format(self.title)
--- pyblog/settings.py
+++ pyblog/settings.py
@@ -131,10 +131,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-CKEDITOR_UPLOAD_PATH = "uploads/"
-#STATIC_DIR = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [STATIC_DIR]
 
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_CONFIGS = {
     'default': {
      'toolbar': 'None'
