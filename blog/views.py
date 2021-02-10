70aff5f849c854dfaf93613ddc38ae35f9919141
denisCenter2m <47447276+DenisYavetsky@users.noreply.github.com>
2021-02-10 11:03:17 +0300

    base comment protect

--- blog/views.py
+++ blog/views.py
@@ -39,13 +39,14 @@ def contacts(request):
             sender = form.cleaned_data['sender']
             message = form.cleaned_data['message']
 
-            #try:
+            # try:
             #    send_mail(subject, message, sender, ['yavetsky@gmail.com'],)
-            #except BadHeaderError:  # Защита от уязвимости
+            # except BadHeaderError:  # Защита от уязвимости
             #    return HttpResponse('Invalid header found')
             # Переходим на другую страницу, если сообщение отправлено
             try:
-                email = EmailMessage(subject, 'From: ' + sender + '\n' + message, to=['yavetsky@gmail.com'], reply_to=[sender])
+                email = EmailMessage(subject, 'From: ' + sender + '\n' + message, to=['yavetsky@gmail.com'],
+                                     reply_to=[sender])
                 email.send()
             except:
                 pass
@@ -54,7 +55,8 @@ def contacts(request):
     else:
         form = ContactForm()
         # Отправляем форму на страницу
-    return render(request, 'contacts.html', context={'posts': posts, 'tags': tags, 'categories': categories, 'form': form})
+    return render(request, 'contacts.html',
+                  context={'posts': posts, 'tags': tags, 'categories': categories, 'form': form})
 
 
 def posts_list(request):
@@ -65,7 +67,6 @@ def posts_list(request):
 
 
 def post_detail(request, slug):
-
     c_form = CommentForm()
     # Получаем все теги и категории для бокового меню
     tags = Tag.objects.all()
@@ -75,22 +76,25 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, slug__iexact=slug)
     comments = Comment.objects.filter(post=post.id)
 
-    #делаем защиту от ботов
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
-    math_res = a + b
+    # if request.method != 'POST':
+    #    a = random.randint(1, 10)
+    #    b = random.randint(1, 10)
+    #    math_res = a + b
 
     if request.method == 'POST':
+
         c_form = CommentForm(request.POST)
         if c_form.is_valid():
-            if int(request.POST['check_sum']) == math_res:
-                if request.POST['name'] not in ['teahGackpeta', 'FurneInosse', 'TiceDuesite', 'excelaysuelapaw', 'excelaysuelapaw', 'Sypepalsslato']:
-                    Comm = c_form.save(commit=False)
-                    Comm.post = post
-                    Comm.save()
-                    post.comment_count = comments.count()
-                    post.save()
-            return redirect(request.path)
+            # if int(request.POST['check_sum']) == math_res:
+            # print('123')
+            if request.POST['name'] not in ['teahGackpeta', 'FurneInosse', 'TiceDuesite', 'excelaysuelapaw',
+                                            'excelaysuelapaw', 'Sypepalsslato']:
+                Comm = c_form.save(commit=False)
+                Comm.post = post
+                Comm.save()
+                post.comment_count = comments.count()
+                post.save()
+        return redirect(request.path)
 
     if not request.session.session_key:
         request.session.save()
@@ -101,7 +105,6 @@ def post_detail(request, slug):
 
     # если нет информации о просмотрах создаем ее
     if is_views.count() == 0 and str(session_key) != 'None':
-
         views = PostCountViews()
         views.sesId = session_key
         views.postId = post
@@ -110,11 +113,9 @@ def post_detail(request, slug):
         post.count_views += 1
         post.save()
 
-    return render(request, 'post_detail.html', context={'post': post, 'tags': tags, 'categories': categories, 'comments': comments, 'c_form': c_form, 'protect': [a, b]})
-
-
-
-
+    return render(request, 'post_detail.html',
+                  context={'post': post, 'tags': tags, 'categories': categories, 'comments': comments,
+                           'c_form': c_form})
 
 
 def tag_detail(request, slug):
@@ -130,8 +131,5 @@ def category_detail(request, slug):
     categories = Category.objects.all()
     category = get_object_or_404(Category, slug__iexact=slug)
 
-    return render(request, 'category_detail.html', context={'category': category, 'tags': tags, 'categories': categories})
-
-
-
-
+    return render(request, 'category_detail.html',
+                  context={'category': category, 'tags': tags, 'categories': categories})
